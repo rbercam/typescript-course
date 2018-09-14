f830f44831a502ec5f5ff82dbcaf0045617616f8
rafael.bercam <rafael.bercam@conta.mobi>
2018-09-14 17:03:37 -0300

    initial commit

---
+++ build/config/env/development.env.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'developmemt',
+    db: 'ts-api',
+    dialect: 'ṕostgres',
+    username: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    serverPort: 3000,
+    pgPort: 5432,
+    dbURL: 'postgres://postgres:pgroot@localhost:5432/ts-api',
+    secret: 'S3cr3t'
+};
---
+++ build/config/env/test.env.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'test',
+    db: 'ts-api-test',
+    dialect: 'ṕostgres',
+    username: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    serverPort: 3000,
+    pgPort: 5432,
+    dbURL: 'postgres://postgres:pgroot@localhost:5432/ts-api-test',
+    secret: 'S3cr3t'
+};
